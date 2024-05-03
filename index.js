fac65d30487632b7613272d2dac2954f26754920
BlueBird08s <raiyan0015@gmail.com>
2024-05-03 23:36:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const BASE_URL = "https://openapi.programming-hero.com/api/retro-forum/posts";
+
+
+
+const discuss = document.querySelectorAll(".fetch first-box");
+
+for(post of posts){
+    console.log(post);
+
+}
+
+    
