681b301698a795fa615de2124fc35a07037cc1fb
BlueBird08s <raiyan0015@gmail.com>
2024-05-05 01:15:31 +0600

    fourth commit

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 
+
 const loadAllData=async (query = "") => {
     const url =`https://openapi.programming-hero.com/api/retro-forum/posts${query}`
+    
     const response = await fetch(url);
-    const dataU = await response.json();
-    // console.log(data);
+    const data = await response.json();
+    const post = data.posts;
+    console.log(post);
 
-    dataU.forEach((data) => {
+    const Fetching = document.getElementById("fetch");
+
+    post.forEach((post1) => {
+        // console.log(post1);
         
-        const div = document.createElement("div");
-div.classList.add("product");
+    const div = document.createElement("div");
+
 div.innerHTML=`
 <div id="fetch-box" class="w-[1000px] h-[150px] bg-slate-200 mr-[200px] rounded-3xl flex gap-10 justify-between items-center" >
 <div>
-    <img src="images/logo.png" alt="">
+    <img src="${post1.image}">;
 </div>
 
 <div>
-    <h1 class="text-2xl font-bold"> News</h1>
-    <p class="text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quis illo voluptatem, excepturi aliquid corporis, iste alias ipsa amet sequi dolores dicta ab facere molestiae quam dolor minus, voluptatibus adipisci!</p>
+    <h1 class="text-2xl font-bold">${post1.title}</h1>
+    <h1 class="text-2xl font-bold">${post1.category}</h1>
+    <p class="text-lg">${post1.description}</p>
 </div>
 
 </div>
@@ -26,33 +33,48 @@ div.innerHTML=`
         Fetching.append(div);
     });
 
-    
-    const Fetching = document.getElementById("fetch");
-
-
-
-
-    
+       
 }
 
 
 const handleSearch=() => {
     const value = document.getElementById("search-box").value;
+    console.log(value);
+    document.getElementById("fetch").innerHTML="";
     loadAllData(`?category=${value}`);
+
 }
-loadAllData(data);
 
-// function loadUser(){
-//     fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
-// .then(Res => Res.json())
-// .then(data => console.log(posts));
-// }
+loadAllData();
 
+const latestData = async () => {
+    const url =`https://openapi.programming-hero.com/api/retro-forum/latest-posts`
+    const response = await fetch(url);
+    const data = await response.json();
 
-// function displayData(data){
-// const discuss = document.getElementById('fetch-box');
-// for(const data of posts){
-//     console.log(data);
-//     }
-// }
-    
+    // console.log(data);
+
+    const latestPost = document.getElementById("latest");
+    data.forEach((data1) => {
+        const div = document.createElement("div");
+
+        div.innerHTML=`
+        <div id="fetch-box" class="w-[1000px] h-[150px] bg-slate-200 mr-[200px] rounded-3xl flex gap-10 justify-between items-center" >
+        <div>
+            <img src="${data1.profile_image}">;
+        </div>
+        
+        <div>
+            <h1 class="text-2xl font-bold">${data1.title}</h1>
+            <h1 class="text-2xl font-bold">${data1.category}</h1>
+            <p class="text-lg">${data1.description}</p>
+        </div>
+        
+        </div>
+        `;
+        latestPost.append(div);
+    })
+
+}
+
+latestData();
